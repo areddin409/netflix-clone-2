fcda4447472e1625def1fbd9984c88460aa2f044
areddin409 <areddin409@gmail.com>
2022-10-03 11:26:19 -0500

    Created Nav Bar

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      {/* Nav  */}
+      <Nav />
+
+      {/* Banner */}
+
+      {/* Row */}
+      {/* Row */}
+      {/* Row */}
+      {/* Row */}
+      {/* Row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqBk8VOJhyte5l_5xBKVId0FXeB7qLMpM",
+  authDomain: "netflix-clone-717fd.firebaseapp.com",
+  projectId: "netflix-clone-717fd",
+  storageBucket: "netflix-clone-717fd.appspot.com",
+  messagingSenderId: "325409031415",
+  appId: "1:325409031415:web:41d8beaf31b0824d6a3ff2"
+};
