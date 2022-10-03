fe01a6a87bcc61b640d41562a00944f1cc8935f1
areddin409 <areddin409@gmail.com>
2022-10-03 13:51:03 -0500

    Created Banner and Row Components

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
+import requests from "../../utils/Requests";
+import Banner from "../Banner/Banner";
 import Nav from "../Nav/Nav";
+import Row from "../Row/Row";
 import "./HomeScreen.css";
 
 function HomeScreen() {
@@ -9,12 +12,21 @@ function HomeScreen() {
       <Nav />
 
       {/* Banner */}
+      <Banner />
 
-      {/* Row */}
-      {/* Row */}
-      {/* Row */}
-      {/* Row */}
-      {/* Row */}
+      {/* Rows */}
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
